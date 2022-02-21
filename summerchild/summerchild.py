288e1a88193edbf67a26345f20e00cf7770ae1e7
Laura Summers <summerscope@gmail.com>
2022-02-21 17:29:13 +1100

    More work on importing json

--- summerchild/summerchild.py
+++ summerchild/summerchild.py
@@ -1,3 +1,11 @@
+import json
+from wsgiref import validate
+io = open("questions.json", "r")
+
+data = json.load(io)
+
+# print (data[0])
+
 def ask_question(text, answers, answer_set, score):
   print(text)
   print(answers)
@@ -14,4 +22,18 @@ def validate_answer(text, answers, answer_set, score):
   while not answered:
     answered = ask_question(text, answers, answer_set, score)
 
-validate_answer('How large is the target cohort for your decision system? \nThe people your system makes decisions, predictions or classifications about.', 'A. 1-100 \nB. 101-1,000 \nC. 1,001 - 10,000', ['A', 'B', 'C'], 0)
+def format_question(question):
+  text = question["text"]
+  answers = question["answers"]
+  answer_list = []
+  for key,value in answers.items():
+    answer_list.append(key + ') ' + value["text"])
+  answers_joined = "\n".join(answer_list)
+  # print (answers_joined)
+  # print (text)
+  validate_answer(text, answers_joined, answers.keys(), 0)
+
+format_question(data[1])
+
+
+# validate_answer('How large is the target cohort for your decision system? \nThe people your system makes decisions, predictions or classifications about.', 'A. 1-100 \nB. 101-1,000 \nC. 1,001 - 10,000', ['A', 'B', 'C'], 0)
